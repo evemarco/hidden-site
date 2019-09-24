c5bce49795fa387cab9e525faa46544eb0a2406e
evemarco <eve.icefr@gmail.com>
2019-09-24 09:31:32 +0200

    page scroll modification

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -2,14 +2,15 @@
   q-layout(view="lHh Lpr lFf")
     q-header(elevated style="background-color: rgba(0, 0, 0, 0.8)")
       q-toolbar(style="background-color: rgba(0, 0, 0, 0)")
-        q-toolbar-title(style="font-family: 'Croissant One', cursive;") Hidden Baguette
-        q-btn(flat).on-left Page 1
-        q-btn(flat).on-left Page 2
-        q-btn(flat).on-left Page 3
-        q-btn(flat).on-left Page 4
+        q-btn(flat no-caps dense size="lg" style="font-family: 'Croissant One', cursive;" @click="$root.$emit('goToSlide', 0)" label="Hidden Baguette")
+        q-btn(flat no-caps @click="$root.$emit('goToSlide', 1)").on-right Page 1
+        q-btn(flat no-caps @click="$root.$emit('goToSlide', 2)").on-right Page 2
+        q-btn(flat no-caps @click="$root.$emit('goToSlide', 3)").on-right Page 3
+        q-btn(flat no-caps @click="$root.$emit('goToSlide', 4)").on-right Page 4
         q-space
-        q-btn(flat @click="openURL('https://forum.hidden-baguette.fr')").on-left Forum
-        div(style="font-family: eve") Eve Online
+        q-btn(flat no-caps icon="img:https://image.flaticon.com/icons/svg/2111/2111370.svg" label="Discord" @click="openURL('https://discord.gg/cvunf46')").on-left
+        q-btn(flat no-caps icon="chat" @click="openURL('https://forum.hidden-baguette.fr')" label="Forum").on-left
+        q-btn(flat no-caps @click="openURL('https://www.eveonline.com/fr/signup?invc=bf6cfbe6-d148-4ac6-ae98-cfd1494b1215')" style="font-family: eve" label="Eve Online")
     q-page-container
       router-view
 </template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,40 @@
 <template lang="pug">
-  .flex.flex-center
+  //- .flex.flex-center
+  div(style="overflow: hidden;")
     .video-background
-      youtube(video-id="sPFII3ozSHI" :mute="true" player-width="100%" player-height="100%" @ready="(player) => log(player)" @ended="(player) => player.target.playVideo()" :player-vars="{ autoplay: 1, loop: 1, controls: 0, showinfo:0, rel: 0, cc_load_policy: 0, cc_lang_pref: 'fr', iv_load_policy: 3, disablekb: 1, fs: 0, modestbranding: 1, autohide: 1 }").video-foreground
-    //- controls:0, showinfo:0, rel: 0, iv_load_policy: 3, disablekb: 1, fs: 0, modestbranding: 1, autohide: 1, loop: 1 }")
-    div(style="width: calc(100vw); height:calc(100vh - 50px); background-color: rgba(0 ,0 ,0 ,0.65)").text-white
-      h1.text-center(style="font-family: 'Croissant One', cursive; padding-top: 10%;") Hidden Baguette
-      h2.text-center Corporation francophone
-        br
-        | Eve Online
-        br
-        | en wormhole space
+      youtube(video-id="sPFII3ozSHI" host="http://www.youtube-nocookie.com" :mute="true" player-width="100%" player-height="100%" @ready="(player) => log(player)" @ended="(player) => player.target.playVideo()" :player-vars="{ autoplay: 1, loop: 1, controls: 0, showinfo:0, rel: 0, cc_load_policy: 0, iv_load_policy: 3, disablekb: 1, fs: 0, modestbranding: 1, autohide: 1, mute: 1, origin: '*' }").video-foreground
+      .dark-background
+    //- controls:0, showinfo:0, rel: 0, iv_load_policy: 3, disablekb: 1, fs: 0, modestbranding: 1, autohide: 1, loop: 1 }") cc_lang_pref: 'fr',
+    #wrap.text-white
+      section
+        transition(appear enter-active-class="animated bounceInLeft delay-2s slow")
+          h1.text-center(key="h1" style="font-family: 'Croissant One', cursive; padding-top: 10%;").no-wrap Hidden Baguette
+        transition(appear enter-active-class="animated bounceInRight delay-2s slow")
+          h2.text-center(key="h2") Corporation francophone
+            br
+            | Eve Online
+            br
+            | en wormhole space
+      section.text-white
+        h2.text-center Page 1
+      section.text-white
+        h2.text-center Page 2
+      section.text-white
+        h2.text-center Page 3
+      section.text-white
+        h2.text-center Page 4
 </template>
 
 <style>
 /* * { box-sizing: border-box; } */
+.dark-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.60);
+  width: 100%;
+  height: 100%;
+}
 .video-background {
   background: #000;
   position: fixed;
@@ -35,20 +56,46 @@
 @media (max-aspect-ratio: 16/9) {
   .video-foreground { width: 300%; left: -100%; }
 }
-@supports (object-fit: cover) {
-  .video-background {
+/* @supports (object-fit: cover) {
+  .video-foreground {
     top: 0; left: 0;
     width: 100%; height: 100%;
     object-fit: cover;
   }
+} */
+.page-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+#wrap {
+  position: relative;
+  overflow: hidden;
+}
+section {
+  box-sizing: border-box;
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+  font-size: 10vmin;
+  padding: 1em;
 }
 </style>
 
 <script>
 import webFont from 'webfontloader'
+import * as FullPageScroll from 'responsive-fullpage-scroll/dist/fullpage-scroll'
 
 export default {
   name: 'PageIndex',
+  data () {
+    return {
+      url: window.location.href,
+      FullPageScroll: FullPageScroll
+    }
+  },
   methods: {
     log (text) {
       console.log(text)
@@ -60,6 +107,20 @@ export default {
         families: ['Croissant One'],
         text: 'Hidden Baguette'
       }
+    })
+  },
+  mounted () {
+    console.log(FullPageScroll)
+    let fps
+    window.addEventListener('load', function () {
+      fps = new this.FullPageScroll('wrap')
+      document.addEventListener('keyup', function (event) {
+        if (event.keyCode === 38) fps.previousSlide()
+        else if (event.keyCode === 40) fps.nextSlide()
+      })
+    })
+    this.$root.$on('goToSlide', (index) => {
+      fps.goToSlide(index)
     })
   }
 }
